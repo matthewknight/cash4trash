a0130ca710d9f820c65c903f10969d06059b5b91
Matthew <mkn29@uclive.ac.nz>
2018-05-16 13:34:21 +1200

    Bids showing + seller details

--- src/Auction.vue
+++ src/Auction.vue
@@ -15,8 +15,25 @@
                 <router-link :to="{ name: 'auctions'}">Back to Auctions</router-link>
                 <br/><br/>
                 <h1>{{ auction.title }}</h1>
+                <span>Start Date: {{ auction.startDateString }} End Date: {{ auction.endDateString }}</span>
+                <h3>Seller Username: {{ seller.username }}</h3>
+                <h3>Seller ID: {{ seller.id }}</h3>
                 <p>{{ auction.description }}</p>
-                <img class="img-responsive" v-bind:src=auction.photoLink>
+                <div style="margin:auto">
+                    <div style="float:left">
+                        <h4> Bids </h4>
+                        <div v-if="bids.length === 0">
+                            <p> No bids yet! </p>
+                        </div>
+                        <div v-else v-for="bid in bids" :key="bid.dateTime">
+                            <p> Amount: {{ bid.amount }} </p>
+                        </div>
+                        
+                    </div>
+                    <div style="float:leftt">
+                        <img class="img-responsive" v-bind:src=auction.photoLink>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -41,26 +58,21 @@
         methods: {
             getAuction: function () {
                 console.log("Auction.vue: Calling getAuction");
-                console.log(this.$route.params.auctionId);
                 this.$http.get('http://localhost:4941/api/v1/auctions/' + this.$route.params.auctionId).then(
                     function (response) {
-                        console.log(response.body);
                         this.auction = response.data;
+                        this.auction['startDateString'] = new Date(this.auction.startDateTime).toDateString();
+                        this.auction['endDateString'] = new Date(this.auction.endDateTime).toDateString();
+                        this.bids = this.auction.bids;
+                        this.seller = this.auction.seller;
                         this.auction["photoLink"] = 'http://localhost:4941/api/v1/auctions/' + this.$route.params.auctionId + "/photos";
-                        this.getSeller();
                     },
                     function (error) {
                         this.error = error;
                         this.errorFlag = true;
                     }
                 )
-            },
-
-            getSeller: function () {
-                console.log("Auction.vue: Calling getSeller");
-
             }
-
         }
     }
 </script>
